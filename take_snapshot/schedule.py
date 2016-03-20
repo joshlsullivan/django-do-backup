c1b023e1b97bb5a28466a683c685ea5c25eafd6c
Josh Sullivan <josh@misllc.com>
2016-03-19 22:50:21 -0500

    updated schedule

--- take_snapshot/schedule.py
+++ take_snapshot/schedule.py
@@ -4,6 +4,6 @@ CELERYBEAT_SCHEDULE = {
     # Executes every Monday morning at 7:30 A.M
     'do-backup-every-monday': {
         'task': 'tasks.do_backup',
-        'schedule': crontab(hour=22, minute=45, day_of_week=0)
+        'schedule': crontab(hour=23, minute=00, day_of_week=0)
     },
 }
